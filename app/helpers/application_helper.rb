7046f0fe5d5e79e15896a8b06ced5817cd61c016
aabiro <aaryn.alexander@gmail.com>
2017-08-19 17:41:37 -0400

    added flash alert generator method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,17 @@
 module ApplicationHelper
-  #use view/application helpers for ruby code logic statements, html use partials
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: style) +
       " ".html_safe +
       (link_to "Login", new_user_session_path, class: style)
     else
-       link_to "Logout", destroy_user_session_path, method: :delete, class: style
-     end
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+    end
   end
 
   def source_helper(layout_name)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout "
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
@@ -59,4 +58,17 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Aaryn Biro's Portfolio", sticky: false)
+  end
+
 end
