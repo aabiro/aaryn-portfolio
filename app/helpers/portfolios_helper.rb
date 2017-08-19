0f782a15c50ed86292ac9ca2e6935457ce06b86b
aabiro <aaryn.alexander@gmail.com>
2017-08-19 16:44:01 -0400

    fixed the forms to include alerts along with fixed portfolio layout style

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,6 +1,7 @@
+
 module PortfoliosHelper
   def image_generator(height:, width:)
-    "http://via.placeholder.com/#{height}x#{width}"
+    "http://placehold.it/#{height}x#{width}"
   end
 
   def portfolio_img img, type
