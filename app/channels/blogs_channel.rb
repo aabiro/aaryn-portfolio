1f181c9193f78945aa1ab8631eb93bb90a8b6619
aabiro <aaryn.alexander@gmail.com>
2017-08-23 12:36:35 -0400

    fixed up cards with gravatar

--- app/channels/blogs_channel.rb
+++ app/channels/blogs_channel.rb
@@ -1,7 +1,8 @@
 class BlogsChannel < ApplicationCable::Channel
 
   def subscribed
-    stream_from "blogs_#{params['blog_id']}_channel"
+    post = "blogs_#{params['blog_id']}_channel"
+    stream_from post
   end
 
   def unsubscribed
