560c836d5255b6c6a58841a260c8f262c5a93085
aabiro <aaryn.alexander@gmail.com>
2017-08-24 13:52:53 -0400

    fixed loading bug

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,2 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+uri = URI.parse(ENV["REDIS_TO_GO_URL"] || "redis://localhost:6379/" )
+REDIS = Redis.new('localhost', :port => 6379, :password => uri.password)
